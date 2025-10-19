5f3e8fa544e307c1d58bfd6b7228f4e5d09dc6f4
Izhar Faruqi <114064928+Izharf@users.noreply.github.com>
2025-10-19 16:21:47 +0530

    Update solution.sql
    seond limit offset query updated

--- 01_second_highest_salary/solution.sql
+++ 01_second_highest_salary/solution.sql
@@ -14,3 +14,14 @@ WHERE salary < (SELECT MAX(salary) FROM employees);
 -- 1. Inner query finds the highest salary.
 -- 2. Outer query finds the max salary that’s smaller than that.
 -- This ensures we skip the top salary and get the next one.
+
+
+-- Second aproach using limit and offset 
+
+
+SELECT DISTINCT salary
+FROM Employee
+ORDER BY salary DESC
+OFFSET 2 ROWS FETCH NEXT 1 ROWS ONLY;
+
+
